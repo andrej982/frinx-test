6227020e236d79a6579adb29f351ee75b39d5a65
Andrej Gafrik <agafrik982@gmail.com>
2022-02-27 12:27:19 +0100

    added db management class

--- __init__.py
+++ __init__.py
@@ -2,21 +2,48 @@ import psycopg2
 import json
 
 
-def connect():
-    connection = psycopg2.connect(
-        host="localhost",
-        database="postgres",
-        user="postgres",
-        password="postgres")
-    return connection
+class Database():
+    def __init__(self):
+        self._connection = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user="postgres",
+            password="postgres")
+        self._cursor = self._connection.cursor()
+
+    @property
+    def connection(self):
+        return self._connection
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def execute(self, query):
+        self.cursor.execute(query)
+
+    def query(self, query):
+        self.cursor.execute(query)
+        return self.fetchall()
+
+
+def configure_db():
+    connection = Database.connection
+    cursor = Database.cursor
 
 
 def read_data():
     with open('configClear_v2.json', 'r') as file:
         data = json.load(file)
         pass
+        return data
+
+
+def parse_data():
+    pass
 
 
 if __name__ == '__main__':
-    conn = connect()
-    read_data()
+    configure_db()
+    network_data = read_data()
+    parse_data(network_data)
